5f60fe1107a15396ff8575bd27cf67b61fb52c97
syedhassaanahmed <hassaan.brix@gmail.com>
2023-01-15 06:02:50 +0100

    Push to GH

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # spark-etl-edge
-A containerized E2E data pipeline which showcases Spark's Structured Streaming capabilities
+A containerized E2E data pipeline which showcases [Spark's Structured Streaming](https://spark.apache.org/docs/latest/structured-streaming-programming-guide.html#overview) capabilities.
+
+## Components
+- [Azure IoT Device Telemetry Simulator](https://github.com/Azure-Samples/Iot-Telemetry-Simulator)
+- [Confluent Community Docker Image for Apache Kafka](https://docs.confluent.io/platform/current/platform-quickstart.html#ce-docker-quickstart) 
+- [Apache PySpark Docker Image](https://hub.docker.com/r/apache/spark-py)
+- [SQL Server 2022 Linux container image](https://learn.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver16&pivots=cs1-bash)
+
+## Architecture
+<div align="center">
+    <img src="./docs/architecture.png">
+</div>
---
+++ main.py
@@ -0,0 +1,66 @@
+import os
+from pyspark.sql import SparkSession, functions as F
+from pyspark.sql.types import *
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName("MyPySparkStreamingApp").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    kafka_broker = os.environ['KAFKA_BROKER']
+    kafka_topic = os.environ['KAFKA_TOPIC']
+
+    df_stream = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_broker) \
+        .option("subscribe", kafka_topic) \
+        .load()
+
+    schema = StructType([
+        StructField("deviceId", StringType()),
+        StructField("time", TimestampType()),
+        StructField("doubleValue", DoubleType())
+    ])
+
+    df_stream = df_stream.selectExpr("CAST(value AS STRING)") \
+        .select(F.from_json(F.col("value"), schema).alias("data")) \
+        .select("data.*")
+
+    windowSpec = F.window("time", "5 seconds")
+
+    # Watermarking handles late arrivals
+    # https://spark.apache.org/docs/latest/structured-streaming-programming-guide.html#handling-late-data-and-watermarking
+    df_stream_agg = df_stream \
+        .withWatermark("time", "5 seconds") \
+        .groupBy(windowSpec, "deviceId") \
+        .agg(F.avg("doubleValue").alias("avgValue")) \
+        .selectExpr("deviceId", "avgValue", "window.start as start", "window.end as end")
+
+    # Uncomment for stdout debugging
+    # query = df_stream_agg \
+    #     .writeStream \
+    #     .outputMode("append") \
+    #     .format("console") \
+    #     .start()
+
+    mssql_host = os.environ['MSSQL_HOST']
+    mssql_sa_password = os.environ['MSSQL_SA_PASSWORD']
+
+    # Using foreachBatch because the sql-spark-connector doesn't directly support writing streams
+    query = df_stream_agg.writeStream \
+        .outputMode("append") \
+        .foreachBatch(lambda df, epoch_id: 
+            df.write \
+                .format("com.microsoft.sqlserver.jdbc.spark") \
+                .mode("append") \
+                # The driver option is buried deep into this issue https://github.com/microsoft/sql-spark-connector/issues/177
+                .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
+                .option("url", f"jdbc:sqlserver://{mssql_host};databaseName=master;") \
+                .option("dbtable", "dbo.telemetry") \
+                .option("user", "sa") \
+                .option("password", mssql_sa_password) \
+                .save()
+        ).start()
+
+    query.awaitTermination()
